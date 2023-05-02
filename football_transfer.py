41c43791b078fa4436eefdbf9692b82240501753
DenyT17 <dan.topa@wp.pl>
2023-05-02 20:18:18 +0200

    animation

--- football_transfer.py
+++ football_transfer.py
@@ -1,12 +1,16 @@
 import pandas as pd
-pd.set_option("mode.chained_assignment", None)
+import matplotlib.pyplot as plt
+import sys
+from matplotlib.animation import FuncAnimation
 from funcitons import salary_calculated, result_calculated, bar_chart_race, sum_expense, top_5
 
+pd.set_option("mode.chained_assignment", None)
 sp = pd.read_csv("Data/Football_transfer/la_liga.csv")
 eng = pd.read_csv("Data/Football_transfer/premier_league.csv")
 it = pd.read_csv("Data/Football_transfer/serie_a.csv")
 de = pd.read_csv("Data/Football_transfer/bundesliga.csv")
 fr = pd.read_csv("Data/Football_transfer/ligue.csv")
+
 
 # la_liga = sum_expense(sp)
 # premier_league = sum_expense(eng)
@@ -14,18 +18,35 @@ fr = pd.read_csv("Data/Football_transfer/ligue.csv")
 # bundesliga = sum_expense(de)
 # ligue = sum_expense(fr)
 
-top_5(sp,"La Liga")
+
 # top_5(eng,"Premier League")
 # top_5(it,"Serie A")
 # top_5(de,"Bundesliga")
 # top_5(fr,"Ligue 1")
+x = []
+y = []
 top_5_data = pd.merge(pd.merge(pd.merge(pd.merge(top_5(sp,"La Liga"),top_5(eng,"Premier League"),on='year'),
                       top_5(it,"Serie A"),on='year'),
-                      top_5(it,"Bundesliga"),on='year'),
+                      top_5(de,"Bundesliga"),on='year'),
                        top_5(fr,"Ligue 1"),on='year')
 
+fig = plt.figure(figsize=(12,8))
+axes = fig.add_subplot(1,1,1)
 
-print(top_5_data)
+def animate(i):
+        x.append(t[i])
+        y.append((y1[i]))
+        plt.plot(x, y, scaley=True, scalex=True, color="blue")
+
+
+
+
+
+
+
+ani = FuncAnimation(fig=fig, func=animate, interval=100)
+
+# bar_chart_race(top_5_data,"Top 5 League clubs expenses")
 # bar_chart_race(premier_league,"Premier League League club expenses")
 # print("premier")
 # bar_chart_race(la_liga,"La Liga League club expenses")
