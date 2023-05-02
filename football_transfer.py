32dece4a16833b1fa4fd1b6cea6c24efeec47afb
DenyT17 <dan.topa@wp.pl>
2023-05-02 21:39:42 +0200

    animation

--- football_transfer.py
+++ football_transfer.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import sys
 from matplotlib.animation import FuncAnimation
+from itertools import count
 from funcitons import salary_calculated, result_calculated, bar_chart_race, sum_expense, top_5
-
+import sys
 pd.set_option("mode.chained_assignment", None)
 sp = pd.read_csv("Data/Football_transfer/la_liga.csv")
 eng = pd.read_csv("Data/Football_transfer/premier_league.csv")
@@ -23,30 +23,7 @@ fr = pd.read_csv("Data/Football_transfer/ligue.csv")
 # top_5(it,"Serie A")
 # top_5(de,"Bundesliga")
 # top_5(fr,"Ligue 1")
-x = []
-y = []
-top_5_data = pd.merge(pd.merge(pd.merge(pd.merge(top_5(sp,"La Liga"),top_5(eng,"Premier League"),on='year'),
-                      top_5(it,"Serie A"),on='year'),
-                      top_5(de,"Bundesliga"),on='year'),
-                       top_5(fr,"Ligue 1"),on='year')
 
-fig = plt.figure(figsize=(12,8))
-axes = fig.add_subplot(1,1,1)
-
-def animate(i):
-        x.append(t[i])
-        y.append((y1[i]))
-        plt.plot(x, y, scaley=True, scalex=True, color="blue")
-
-
-
-
-
-
-
-ani = FuncAnimation(fig=fig, func=animate, interval=100)
-
-# bar_chart_race(top_5_data,"Top 5 League clubs expenses")
 # bar_chart_race(premier_league,"Premier League League club expenses")
 # print("premier")
 # bar_chart_race(la_liga,"La Liga League club expenses")
@@ -57,3 +34,43 @@ ani = FuncAnimation(fig=fig, func=animate, interval=100)
 # print("serie a")
 # bar_chart_race(ligue,"Ligue 1 League club expenses")
 # print("ligue")
+
+top_5_data = pd.merge(pd.merge(pd.merge(pd.merge(top_5(sp,"La Liga"),top_5(eng,"Premier League"),on='year'),
+                      top_5(it,"Serie A"),on='year'),
+                      top_5(de,"Bundesliga"),on='year'),
+                       top_5(fr,"Ligue 1"),on='year')
+
+
+x=[]
+y=[]
+
+fig,ax=plt.subplots()
+ax.plot(x,y)
+ax.set_xlim(0, len(top_5_data))
+ax.set_ylim(0, top_5_data["Premier League"].max() * 1.04)
+ax.set_xlabel('Year')
+ax.set_ylabel('League expenses')
+ax.set_title('Top 5 Leagues expenses')
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+fig.set_size_inches(12.8, 7.2)
+
+counter=count(0,1)
+
+# Get the shape of the DataFrame
+n_rows, n_cols = top_5_data.shape
+
+def update(i):
+    idx=next(counter)
+    try:
+      x.append(top_5_data.index[idx])
+      y.append(top_5_data.iloc[idx])
+    except IndexError:
+       print("Index out of range!")
+       sys.exit(1)
+    plt.cla()
+    ax.plot(x,y)
+
+
+ani=FuncAnimation(fig=fig,func=update,interval=400)
+plt.show()
