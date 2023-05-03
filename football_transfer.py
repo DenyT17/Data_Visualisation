caffbbc4465f05d9dbb8771112fe67f30b9603d7
DenyT17 <dan.topa@wp.pl>
2023-05-03 19:44:24 +0200

    Top 5

--- football_transfer.py
+++ football_transfer.py
@@ -1,9 +1,7 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from itertools import count
-from funcitons import salary_calculated, result_calculated, bar_chart_race, sum_expense, top_5
-import sys
+
+from funcitons import salary_calculated, result_calculated, bar_chart_race, sum_expense, top_5, line_plot
+
 pd.set_option("mode.chained_assignment", None)
 sp = pd.read_csv("Data/Football_transfer/la_liga.csv")
 eng = pd.read_csv("Data/Football_transfer/premier_league.csv")
@@ -41,36 +39,4 @@ top_5_data = pd.merge(pd.merge(pd.merge(pd.merge(top_5(sp,"La Liga"),top_5(eng,"
                        top_5(fr,"Ligue 1"),on='year')
 
 
-x=[]
-y=[]
-
-fig,ax=plt.subplots()
-ax.plot(x,y)
-ax.set_xlim(0, len(top_5_data))
-ax.set_ylim(0, top_5_data["Premier League"].max() * 1.04)
-ax.set_xlabel('Year')
-ax.set_ylabel('League expenses')
-ax.set_title('Top 5 Leagues expenses')
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
-fig.set_size_inches(12.8, 7.2)
-
-counter=count(0,1)
-
-# Get the shape of the DataFrame
-n_rows, n_cols = top_5_data.shape
-
-def update(i):
-    idx=next(counter)
-    try:
-      x.append(top_5_data.index[idx])
-      y.append(top_5_data.iloc[idx])
-    except IndexError:
-       print("Index out of range!")
-       sys.exit(1)
-    plt.cla()
-    ax.plot(x,y)
-
-
-ani=FuncAnimation(fig=fig,func=update,interval=400)
-plt.show()
+line_plot(top_5_data)
--- funcitons.py
+++ funcitons.py
@@ -1,6 +1,12 @@
 import pandas as pd
+from matplotlib import style
 import bar_chart_race as bcr
-
+from matplotlib.animation import FuncAnimation, FFMpegWriter
+from itertools import count
+import sys
+import matplotlib.pyplot as plt
+from matplotlib.artist import Artist
+import time
 def salary_calculated(x):
     if "Loan fee:€" in x["fee"]:
         if "m" in x["fee"]:
@@ -83,3 +89,83 @@ def top_5(data,league_name):
     data = data[["League", "year", "Sum"]].pivot_table(index="year", columns="League",
                                                           values="Sum")
     return data
+
+
+
+def line_plot(data):
+    x = []
+    Serie_A = []
+    La_Liga = []
+    Premier_League = []
+    Bundesliga = []
+    Ligue = []
+    plt.style.use('dark_background')
+    fig, ax = plt.subplots(figsize=(12,5))
+    ax.plot(x, Serie_A, linewidth=5, color="b", label='Serie A')
+    ax.plot(x, La_Liga, linewidth=5, color="r", label='La Liga')
+    ax.plot(x, Premier_League, linewidth=5, color="g", label='Premier League')
+    ax.plot(x, Bundesliga, linewidth=5, color="y", label='Bundesliga ')
+    ax.plot(x, Ligue, linewidth=5, color="c", label='Ligue 1')
+    ax.set_xlim(1990,2023)
+    ax.set_ylim(10**6,3*10**10)
+    ax.legend(loc='upper left')
+    ax.set_xlabel('Year')
+    ax.set_ylabel('League expenses')
+    ax.set_title('Top 5 Leagues expenses')
+    counter = count(0, 1)
+
+    Serie_A_text = ax.annotate(
+        "Serie A", xy=(0,0),xytext=(0, 0),)
+    La_Liga_text = ax.annotate(
+        "La Liga", xy=(0,0),xytext=(0, 0),)
+    Premier_League_text = ax.annotate(
+        "Premier League", xy=(0,0),xytext=(0, 0),)
+    Bundesliga_text = ax.annotate(
+        "Bundesliga", xy=(0,0),xytext=(0, 0),)
+    Ligue_text = ax.annotate(
+        "Ligue", xy=(0,0),xytext=(0, 0),)
+    def update(i):
+        idx = next(counter)
+        try:
+            x.append(data.index[idx])
+            Serie_A.append(data["Serie A"].iloc[idx])
+            La_Liga.append(data["La Liga"].iloc[idx])
+            Premier_League.append(data["Premier League"].iloc[idx])
+            Bundesliga.append(data["Bundesliga"].iloc[idx])
+            Ligue.append(data["Ligue 1"].iloc[idx])
+
+            Serie_A_text.xy = (1991,1.8 *10**10)
+            Serie_A_text.set_position((1991,1.8 *10**10))
+            Serie_A_text.set_text(f"Serie A - {round(round(Serie_A[-1])/10**6,1)} milion €")
+
+            La_Liga_text.xy = (1991, 1.6 * 10 ** 10)
+            La_Liga_text.set_position((1991, 1.6 * 10 ** 10))
+            La_Liga_text.set_text(f"La Liga - {round(round(La_Liga[-1]) / 10 ** 6,1)} milion €")
+
+            Premier_League_text.xy = (1991, 1.4 * 10 ** 10)
+            Premier_League_text.set_position((1991, 1.4 * 10 ** 10))
+            Premier_League_text.set_text(f"Premier League - {round(round(Serie_A[-1]) / 10 ** 6,1)} milion €")
+
+            Bundesliga_text.xy = (1991, 1.2 * 10 ** 10)
+            Bundesliga_text.set_position((1991, 1.2 * 10 ** 10))
+            Bundesliga_text.set_text(f"Bundesliga - {round(round(Serie_A[-1]) / 10 ** 6,1)} milion €")
+
+            Ligue_text.xy = (1991, 1 * 10 ** 10)
+            Ligue_text.set_position((1991, 1 * 10 ** 10))
+            Ligue_text.set_text(f"Ligue 1 - {round(round(Serie_A[-1]) / 10 ** 6, 1)} milion €")
+        except IndexError:
+            time.sleep(2)
+            sys.exit(1)
+
+
+        ax.plot(x, Serie_A,linewidth=5, color="b", label='Serie A')
+        ax.plot(x, La_Liga,linewidth=5, color="r",label='La Liga')
+        ax.plot(x, Premier_League,linewidth=5, color="g",label='Premier League')
+        ax.plot(x, Bundesliga,linewidth=5, color="y",label='Bundesliga ')
+        ax.plot(x, Ligue,linewidth=5, color="c",label='Ligue 1')
+
+
+    ani = FuncAnimation(fig=fig, func=update, interval=400)
+    # plt.show()
+    FFwriter = FFMpegWriter(fps=3)
+    ani.save('Top_5.mp4', writer=FFwriter)
